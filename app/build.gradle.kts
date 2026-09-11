96ab8280687835c1f11d6a6929129e3620ef9f69
Bolin <Bolin8621@gmail.com>
2026-09-11 23:24:11 +0800

    Bump app version to 0.1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.bolin.photohelper"
         minSdk = 31
         targetSdk = 34
-        versionCode = 2
-        versionName = "0.1.1"
+        versionCode = 3
+        versionName = "0.1.2"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
         val envFile = rootProject.file(".env")
